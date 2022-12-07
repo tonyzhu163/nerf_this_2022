7bf75284dc215c85390e87827e67c630426b8ad7
JiPingX <mingxuan_ji@brown.edu>
2022-12-07 17:23:07 -0500

    get_embedder, and create_nerf (and its dependencies)

--- code/model.py
+++ code/model.py
@@ -2,6 +2,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from .Encoders import get_embedder
+
+## We always assume that our model uses 5d inputs, so we will not use
+## the "use_viewdir" argument as in the codes we reference.
+
 
 class NeRF(nn.Module):
     def __init__(self, D=8, W=256, posi_len=60, dir_len=24, skips=[4]):
@@ -55,3 +60,114 @@ class NeRF(nn.Module):
         rgb = self.rgb_linear(h)
         outputs = torch.cat([rgb, density], -1)
         return outputs
+
+def batchify(fn, chunk):
+    """Constructs a version of 'fn' that applies to smaller batches.
+    """
+    if chunk is None:
+        return fn
+    def ret(inputs):
+        return torch.cat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
+    return ret
+
+def run_network(inputs, viewdirs, fn, embed_fn_pos, embed_fn_dir, netchunk=1024*64):
+    inputs_flat = torch.reshape(inputs, [-1, inputs.shape[-1]])
+    embedded_pos = embed_fn_pos(inputs_flat)
+
+    input_dirs = viewdirs[:,None].expand(inputs.shape)
+    input_dirs_flat = torch.reshape(input_dirs, [-1, input_dirs.shape[-1]])
+    embedded_dirs = embed_fn_dir(input_dirs_flat)
+    embedded = torch.cat([embedded_pos, embedded_dirs], -1)
+
+    outputs_flat = batchify(fn, netchunk)(embedded)
+    output_shape = list(inputs.shape[:-1])
+    output_shape.append(outputs_flat.shape[-1])
+    outputs = torch.reshape(outputs_flat,output_shape)
+    return outputs
+
+    
+
+def create_nerf(args, device):
+    embed_fn_pos, input_ch_pos = get_embedder(args.multires, args, i_embed=args.i_embed)
+    ### if args.i_embed==1:
+    embed_fn_dir, input_ch_dir = get_embedder(args.multires_views, args, i_embed=args.i_embed_views)
+    skips = [4]
+    if args.i_embed==1:
+        ## instant NGP
+        pass
+    else:
+        model = NeRF(D=args.netdepth, W=args.netwidth,
+                posi_len=input_ch_pos, dir_len=input_ch_dir, skips=skips).to(device)
+    grad_vars = list(model.parameters())
+
+    model_fine = None
+
+    if args.N_importance > 0:
+        if args.i_embed==1:
+            ##
+            pass
+        else:
+            model_fine = NeRF(D=args.netdepth_fine, W=args.netwidth_fine,
+                posi_len=input_ch_pos, dir_len=input_ch_dir, skips=skips).to(device)
+        grad_vars += list(model_fine.parameters())
+    
+    #################### need to modify
+    network_query_fn = lambda inputs, viewdirs, network_fn : run_network(inputs, viewdirs, network_fn,
+                        embed_fn_pos=embed_fn_pos, embed_fn_dir=embed_fn_dir, netchunk=args.netchunk)
+
+    if args.i_embed==1:
+        ##
+        pass
+    else:
+        optimizer = torch.optim.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
+
+    
+    start = 0
+    basedir = args.basedir
+    expname = args.expname
+    '''
+    ##########################
+
+    # Load checkpoints
+    if args.ft_path is not None and args.ft_path!='None':
+        ckpts = [args.ft_path]
+    else:
+        ckpts = [os.path.join(basedir, expname, f) for f in sorted(os.listdir(os.path.join(basedir, expname))) if 'tar' in f]
+
+    print('Found ckpts', ckpts)
+    if len(ckpts) > 0 and not args.no_reload:
+        ckpt_path = ckpts[-1]
+        print('Reloading from', ckpt_path)
+        ckpt = torch.load(ckpt_path)
+
+        start = ckpt['global_step']
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+
+        # Load model
+        model.load_state_dict(ckpt['network_fn_state_dict'])
+        if model_fine is not None:
+            model_fine.load_state_dict(ckpt['network_fine_state_dict'])
+        if args.i_embed==1:
+            embed_fn.load_state_dict(ckpt['embed_fn_state_dict'])
+
+    ##########################
+    '''
+    render_kwargs_train = {
+        'network_query_fn' : network_query_fn,
+        'perturb' : args.perturb,
+        'N_importance' : args.N_importance,
+        'network_fine' : model_fine,
+        'N_samples' : args.N_samples,
+        'network_fn' : model,
+        'embed_fn_dir': embed_fn_dir,
+        'embed_fn_pos': embed_fn_pos,
+        'white_bkgd' : args.white_bkgd,
+        'raw_noise_std' : args.raw_noise_std,
+    }
+
+    render_kwargs_test = {k : render_kwargs_train[k] for k in render_kwargs_train}
+    render_kwargs_test['perturb'] = False
+    render_kwargs_test['raw_noise_std'] = 0.
+
+    return render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer
+
