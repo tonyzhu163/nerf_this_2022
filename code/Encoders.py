6fcf71fae6b2eb6824724f2e3c31c8f0008fbfba
JiPingX <mingxuan_ji@brown.edu>
2022-12-07 17:23:49 -0500

    get_embedder, and create nerf (and its dependencies)

--- code/Encoders.py
+++ code/Encoders.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import numpy as np
 
 class FreqEncoder(nn.Module):
-    def __init__(self, max_freq=10, num_freqs=10, d=3): #TEMP
+    def __init__(self, max_freq=9, num_freqs=10, d=3): #TEMP
         super(FreqEncoder, self).__init__()
         periodics = [torch.sin, torch.cos]
-        freq_bands = 2.**torch.linspace(0., max_freq-1, steps=num_freqs)
+        freq_bands = 2.**torch.linspace(0., max_freq, steps=num_freqs)
         encoder_fns = [lambda x, fn=fn, freq=freq: fn(x*freq)
                        for freq in freq_bands
                        for fn in periodics]
@@ -17,3 +17,11 @@ class FreqEncoder(nn.Module):
     def forward(self, x):
         return torch.cat([fn(x) for fn in self.encoder_fns], -1)
 
+
+def get_embedder(multries, args, i_embed = 0):
+    if i_embed == 0:
+        embedder_obj = FreqEncoder(multries-1,multries)
+        embed = lambda x, eo=embedder_obj : eo.forward(x)
+        out_dim = embedder_obj.out_dim
+    
+    return embed, out_dim
