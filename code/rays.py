6100be2f612061338c2d2288cf9eb9608845d741
tonyzhu163 <tonyzhu163@gmail.com>
2022-12-07 15:43:34 -0500

    update comments

--- code/rays.py
+++ code/rays.py
@@ -8,16 +8,28 @@ def generate_rays(H, W, focal, pose):
     x, y = x.t(), y.t()
 
     # equation from https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-generating-camera-rays/generating-camera-rays
+    # may or may not need 0.5 shift
 
-    dx = (x + 0.5 -W/2)/focal
-    dy = -(y + 0.5 -H/2)/focal
+    dx = (x + 0.5 - W/2) / focal
+    dy = -(y + 0.5 - H/2) / focal
     dir = torch.stack([dx, dy, -torch.ones_like(x)], -1)
-    
-    ray_d = ray_d / (torch.norm(dir, dim=-1, keepdim=True) + 1e-10)
-    ray_d = torch.sum(ray_d[..., np.new_axis, :] * pose[:3, :3], -1)
+
+    ray_d = torch.sum(dir[..., np.new_axis, :] * pose[:3, :3], -1)
+    ray_d = ray_d / (torch.norm(ray_d, dim=-1, keepdim=True) + 1e-10)
     ray_o = pose[:3, -1].expand(ray_d.shape)
 
     return ray_d, ray_o
 
 
 def render(H, W, focal, max_rays, rays, pose, near, far):
+    # section 4 in NERF
+
+    generate_rays(H, W, focal, pose)
+
+    # batchify_ray -> render_ray -> h_sampling
+
+def h_sampling():
+    # section 5.2 in NERF
+
+
+
--- code/run.py
+++ code/run.py
@@ -1,11 +1,15 @@
 import os
 from pathlib import Path
 
+import torch
+
 from .load_blender import load_blender_data
 from .rays import generate_rays, render
 
 def main():
-    data_dir = Path.cwd() / '_data' / 'nerf_synthetic'
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    data_dir = Path.cwd().parent / '_data' / 'nerf_synthetic'
     images, poses, render_poses, hwf, i_split = load_blender_data(data_dir)
     print('Loaded blender', images.shape, render_poses.shape, hwf, data_dir)
     i_train, i_val, i_test = i_split
