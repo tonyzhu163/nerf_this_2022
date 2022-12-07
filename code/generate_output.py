3e51a789b8ab57e2209bb239ed84c2ced84b1c4e
Andrew X. Ding <andrewdingcanada8@gmail.com>
2022-12-07 16:18:31 -0500

    add placeholder generate_output file

---
+++ code/generate_output.py
@@ -0,0 +1,22 @@
+import torch
+
+#TODO: This entire class needs to be refactored
+
+def generate_output():
+    print('SET TO RENDER ONLY')
+    print('Rendering now...')
+    with torch.no_grad():
+        if args.render_test:
+            # render_test switches to test poses
+            images = images[i_test]
+        else:
+            # Default is smoother render_poses path
+            images = None
+
+        testsavedir = os.path.join(basedir, expname, 'renderonly_{}_{:06d}'.format('test' if args.render_test else 'path', start))
+        os.makedirs(testsavedir, exist_ok=True)
+        print('test poses shape', render_poses.shape)
+
+        rgbs, _ = render_path(render_poses, hwf, K, args.chunk, render_kwargs_test, gt_imgs=images, savedir=testsavedir, render_factor=args.render_factor)
+        print('Done rendering', testsavedir)
+        imageio.mimwrite(os.path.join(testsavedir, 'video.mp4'), to8b(rgbs), fps=30, quality=8)
--- code/run.py
+++ code/run.py
@@ -6,7 +6,7 @@ import torch
 from load_blender import load_blender_data
 from rays import generate_rays, render
 from params import get_params
-
+from generate_output import generate_output
 def main():
     params = get_params()
     
@@ -17,7 +17,11 @@ def main():
     print('Loaded blender', images.shape, render_poses.shape, hwf, data_dir)
     i_train, i_val, i_test = i_split
 
-    # 
+    # Short circuit if only rendering out from trained model
+    if params.render_only:
+        # generate_output()
+        return
 
+    
 if __name__ == "__main__":
     main()
