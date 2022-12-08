9e0172960b89623d0abebdd579ebf725fc13e035
tonyzhu163 <tonyzhu163@gmail.com>
2022-12-07 23:55:44 -0500

    params comments

--- code/params.py
+++ code/params.py
@@ -9,17 +9,18 @@ class ModelParameters:
     # expname:
     # basedir: './logs/'
     datadir: list[str] #* value defined below to avoid defaultfactory hassle
-    epochs: int = 200000
+    epochs: int = 10000
+    # pixel_nerf 500 -> 5 min aim for 10000 for now
     
     netdepth: int = 8
     netwidth: int = 256
     netdepth_fine: int = 8
     netwidth_fine: int = 256
-    N_rand: int = 32*32*4 #* N_rand is the number of rays in the batch
+    N_rand: int = 32*32*4 #* N_rand is the number of rays in the batch, This should be ray_batch_size
     lrate: float = 5e-4
     lrate_decay: int = 250
-    chunk: int = 1024*32
-    netchunk: int = 1024*64
+    chunk: int = 1024*32 # This should be max_ray
+    netchunk: int = 1024*64 # This should be max_points
     use_batching: bool = False
     # no_reload:
     # ft_path:
@@ -28,7 +29,7 @@ class ModelParameters:
     N_samples: int = 64 #* N_samples is number of coarse samples per ray
     N_importance: int = 0 #* N_importance if >= 1, increases the number of bins in the fine sample (only called on the fine NERF once)
     perturb: int = 1.
-    # use_viewdirs:
+    # use_viewdirs: I think we assume this is always positive
     # i_embed:
     multires: int = 10
     multires_views: int = 4
@@ -50,11 +51,11 @@ class ModelParameters:
     # spherify:
     # llffhold:
     ## logging/saving options
-    # i_print:
-    # i_img:
-    # i_weights:
+    # i_print: number of epochs per console printout
+    # i_img: number of epochs per tensorboard output
+    # i_weights: number of epochs per checkpoint
     # i_testset:
-    # i_video:
+    # i_video: number of epochs per render
 
 def get_params():
     params = ModelParameters(datadir=['data', 'nerf_synthetic'])
