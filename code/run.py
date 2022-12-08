c1151d55d0efffbb7f8a0387380ab4c80294a300
Andrew X. Ding <andrewdingcanada8@gmail.com>
2022-12-07 22:43:04 -0500

    add update_lr helper function, create_nerf placeholder

--- code/run.py
+++ code/run.py
@@ -2,11 +2,31 @@ import os
 from pathlib import Path
 
 import torch
+from tqdm import tqdm, trange
 
 from load_blender import load_blender_data
 from rays import generate_rays, render
 from params import get_params
 from generate_output import generate_output
+from numpy import clip, uint8
+
+def create_nerf():
+    pass  # placeholder for create_nerf in model.py
+
+img2mse = lambda x, y : torch.mean((x - y) ** 2)
+mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+# to8b = lambda x : (255*clip(x,0,1)).astype(uint8) #TODO: why can't we use torch here
+to8b = lambda x : (255*torch.clip(x,0,1)).astype(torch.uint8) #TODO: why can't we use torch here
+
+
+
+def update_lr(params, optimizer, global_step):
+    decay_rate = 0.1
+    decay_steps = params.lrate_decay * 1000
+    new_lrate = params.lrate * (decay_rate ** (global_step / decay_steps))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = new_lrate
+    
 def main():
     params = get_params()
 
