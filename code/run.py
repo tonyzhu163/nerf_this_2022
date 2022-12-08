904a994941833b633a2b2bb6cb946d9e31f6f3eb
Andrew X. Ding <andrewdingcanada8@gmail.com>
2022-12-07 22:43:43 -0500

    finish v1 of training loop

--- code/run.py
+++ code/run.py
@@ -39,11 +39,52 @@ def main():
     print("Loaded blender", images.shape, render_poses.shape, H, W, F, K, data_dir)
     i_train, i_val, i_test = i_split
 
+    render_poses = torch.Tensor(render_poses).to(device)
+
+    #TODO: can we attach optimizer to render_kwargs or smth damn
+    render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer = create_nerf(
+        params
+    )
+    global_step = start
+
+    for kwargs in [render_kwargs_train, render_kwargs_test]:
+        kwargs["near"] = near
+        kwargs["far"] = far
+
     # Short circuit if only rendering out from trained model
     if params.render_only:
         # generate_output()
         return
+    # TODO: implement batching in some other python file
+    batch_rays, target_s = None, None
+    for epoch in trange(start + 1, params.epochs + 1):
+        rgb, disp, acc, extras = render( #TODO: need to modify this so the render function matches
+            H,
+            W,
+            K,
+            chunk=params.chunk,
+            rays=batch_rays,
+            verbose=epoch < 10,
+            retraw=True,
+            **render_kwargs_train
+        )
+        optimizer.zero_grad()
+        
+        loss = torch.mean((rgb - target_s)**2) #* mean squared error as tensor
+        psnr = mse2psnr(loss) #* peak signal-to-noise ratio as tensor
+                
+        loss.backward()
+        optimizer.step()
 
-    
+        update_lr(params, optimizer, global_step)
+        if epoch%params.i_weights==0: pass
+        if epoch%params.i_video==0 and epoch > 0: pass
+        if epoch%params.i_testset==0 and epoch > 0: pass
+        
+        if epoch%params.i_print==0:
+            tqdm.write(f"[TRAIN] Iter: {epoch} Loss: {loss.item()}  PSNR: {psnr.item()}")
+        global_step += 1
+
+
 if __name__ == "__main__":
     main()
