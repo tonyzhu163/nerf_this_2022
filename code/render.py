531befc6800b6bddec75f8c620a89354d273f9c3
tonyzhu163 <tonyzhu163@gmail.com>
2022-12-08 23:24:56 -0500

    render 1.4

--- code/render.py
+++ code/render.py
@@ -1,13 +1,15 @@
 import torch
 
+from collections import defaultdict
 from rays import generate_rays, sample_coarse, sample_fine
+from raw2outputs import raw2outputs
+from model import run_network
 
 
 def render(H, W, K, max_rays, rays, near, far, pose=None, **kwargs):
     # section 4 in NERF
 
     # --- Prepare Input ---
-
     # test pose is provided for render_path (testing)
     if pose is not None:
         rays_d, rays_o = generate_rays(H, W, K, pose)
@@ -21,27 +23,23 @@ def render(H, W, K, max_rays, rays, near, far, pose=None, **kwargs):
     # near and far from ** kwargs?
     near, far = near * torch.ones_like(rays_d[..., :1]), far * torch.ones_like(rays_d[..., :1])
 
-    # [H*W, 3 + 3 + 1 + 1]
-    rays = torch.cat([rays_o, rays_d, near, far], -1)
-
     # --- Input Rays ---
     # rays: (N_rays, 3+3+2), ray origins, directions and near, far depth bounds
-    # feel like we are repeating data here so this isn't neccesary
-    # viewdirs = rays_d
-    # rays = torch.cat([rays, viewdirs], -1)
+    rays = torch.cat([rays_o, rays_d, near, far], -1)
 
-    # --- OUTPUT DEPENDS ON BATCHIFY RAY ---
+    # --- Render Ray by Batch ---
+    records = defaultdict(list)
+    for i in range(0, rays.shape[0], max_rays):
+        ret = render_ray(rays[i:i + max_rays, :], **kwargs)
+        for k in ret:
+            records[k].append(ret[k])
 
-    return
+    records = {k: torch.cat(records[k], 0) for k in records}
+    # why do we need to reshape here?
 
-
-def batchify_ray(rays_flat, chunk=1024*32, **kwargs):
-    for i in range(0, rays_flat.shape[0], chunk):
-        ret = render_ray(rays_flat[i:i + chunk], **kwargs)
-
-    # --- OUTPUT DEPENDS ON RENDER_RAY ---
-
-    return
+    inf = ['rgb_map', 'disp_map', 'acc_map']
+    output = {i: records[inf] for i in inf}
+    return output
 
 
 def render_ray(rays, n_samples, n_importance=0, perturb=0, chunk=1024*32):
@@ -56,8 +54,23 @@ def render_ray(rays, n_samples, n_importance=0, perturb=0, chunk=1024*32):
 
     pts_coarse = sample_coarse(z_vals, z_vals_mid, rays_o, rays_d, perturb)
 
+    # --- NOT DONE ---
+    raw = run_network(pts_coarse, rays_d, )
+    rgb_map, disp_map, acc_map, weights, depth_map = raw2outputs(raw, )
+
     if n_importance > 0:
-        pts_fine = sample_fine(z_vals, z_vals_mid, rays_o, rays_d, weights, n_importance, perturb)
+        rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
+        pts_fine, z_samples = sample_fine(z_vals, z_vals_mid, rays_o, rays_d, weights, n_importance, perturb)
 
+        # --- NOT DONE ---
+        raw = run_network(pts_fine, rays_d, )
+        rgb_map, disp_map, acc_map, _, _ = raw2outputs(raw, )
 
-    return
+    ret = {'rgb_map': rgb_map, 'disp_map': disp_map, 'acc_map': acc_map}
+    if n_importance > 0:
+        ret['rgb0'] = rgb_map_0
+        ret['disp0'] = disp_map_0
+        ret['acc0'] = acc_map_0
+        ret['z_std'] = torch.std(z_samples, dim=-1, unbiased=False)
+
+    return ret
