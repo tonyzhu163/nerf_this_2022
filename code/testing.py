5945305fb3ec3ba3ceb5e3f499b739cfb4a148e7
tonyzhu163 <tonyzhu163@gmail.com>
2022-12-12 20:52:08 -0500

    fix typo

--- code/testing.py
+++ code/testing.py
@@ -20,9 +20,9 @@ def test_weights(model: NeRF, model_fine: NeRF, test_size, repeat_test, weights_
     for x in p.rglob('*.tar'):
         weights.append(x)
 
-    loss_lst_f = []
-    psnr_lst_f = []
-    psnr0_lst_f = []
+    loss_lst = []
+    psnr_lst = []
+    psnr0_lst = []
 
     for w in weights:
         psnr0 = None
@@ -38,9 +38,9 @@ def test_weights(model: NeRF, model_fine: NeRF, test_size, repeat_test, weights_
         # if model_fine is not None:
         #     model_fine.load_state_dict(ckpt['network_fine_state_dict'])
 
-        loss_lst = []
-        psnr_lst = []
-        psnr0_lst = []
+        loss_avg = []
+        psnr_avg = []
+        psnr0_avg = []
 
         for n in range(repeat_test):
 
@@ -58,19 +58,19 @@ def test_weights(model: NeRF, model_fine: NeRF, test_size, repeat_test, weights_
                 loss = loss + loss0
                 psnr0 = mse2psnr(loss0)
 
-            loss_lst.append(loss.item())
-            psnr_lst.append(psnr.item())
+            loss_avg.append(loss.item())
+            psnr_avg.append(psnr.item())
 
             if psnr0:
-                psnr0_lst.append(psnr0.item())
+                psnr0_avg.append(psnr0.item())
 
-        loss_lst_f.append(np.mean(loss_lst))
-        psnr_lst_f.append(np.mean(psnr.avg))
-        psnr0_lst_f.append(np.mean(psnr.avg))
+        loss_lst.append(np.mean(loss_avg))
+        psnr_lst.append(np.mean(psnr_avg))
+        psnr0_lst.append(np.mean(psnr0_avg))
 
-    ret['loss'] = loss_lst_f
-    ret['psnr'] = psnr_lst_f
-    ret['psnr0'] = psnr0_lst_f
+    ret['loss'] = loss_lst
+    ret['psnr'] = psnr_lst
+    ret['psnr0'] = psnr0_lst
     ret['epoch'] = epochs
     return ret
 
